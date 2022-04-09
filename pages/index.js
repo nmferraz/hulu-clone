5cdec42e82f182650cebc9a217eef88231f9374c
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-09 23:52:21 +0100

    Navbar Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Navbar />
     </div>
   )
 }
